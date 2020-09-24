4aa23f91701ed1a7908934e4cd3bd2db92d4a51b
Erik Sundberg <sten.erik.sundberg@gmail.com>
2020-09-24 14:42:40 +0200

    Add project files.

---
+++ Projektarbete/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projektarbete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
