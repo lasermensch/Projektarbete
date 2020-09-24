6bc1af3dddfb9e45bbf790fde2c8a5a3af34b784
Lucas <lucas.ersson@student.kyh.se>
2020-09-24 15:34:50 +0200

    kommentar

--- Projektarbete/Program.cs
+++ Projektarbete/Program.cs
@@ -9,6 +9,7 @@ namespace Projektarbete
             Console.WriteLine("Hello World!");
             Console.WriteLine("Tjena tjena");
             Console.WriteLine("All your base are belong to us!");
+            //Det funkar niceeeeeee
         }
     } //André testar läggga till en kommentar
 
