de94f30a5b7ed32d4eb956b679062c0f2b279bb2
Lucas <lucas.ersson@student.kyh.se>
2020-09-29 10:45:10 +0200

    added classes

--- Projektarbete/Program.cs
+++ Projektarbete/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Projektarbete
 {
@@ -14,5 +15,24 @@ namespace Projektarbete
             Console.WriteLine("testar igen");
         }
     } //André testar läggga till en kommentar
+
+    class Entities
+    {
+        public int Health;
+        public int Strenght;
+        public int Armour;
+        public int Toughness;
+        public int Initiative;
+    }
+
+    class Player : Entities
+    {
+        //Backpack list
+    }
+    class Enemy : Entities
+    {
+
+    }
+
 
 }
