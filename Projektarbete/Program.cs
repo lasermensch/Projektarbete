a63ab76a7d0fe3442f080916303bc9c8811ed1a8
Erik Sundberg <sten.erik.sundberg@gmail.com>
2020-10-01 18:48:47 +0200

    Implementerad kod för möte

--- Projektarbete/Program.cs
+++ Projektarbete/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Threading;
 
 namespace Projektarbete
 {
@@ -21,7 +22,7 @@ namespace Projektarbete
         static void StartGame(World world)
         {
             InstantiatePlayer(world);
-            InstantiateEnemies(world);
+            
         }
 
         static void InstantiatePlayer(World world)
@@ -31,15 +32,12 @@ namespace Projektarbete
             character.Strength = 2;
             character.Armour = 4;
 
-            PlayerMovement(world);
+            PlayerMovement(world, character);
         }
 
-        static void InstantiateEnemies(World world)
-        {
-            var enemies = new List<int>();
-        }
+        
 
-        static void PlayerMovement(World world) //Kodstycke som säger åt konsolen hur "spelaren" rör sig runt på spelplanen.
+        static void PlayerMovement(World world, Player character) //Kodstycke som säger åt konsolen hur "spelaren" rör sig runt på spelplanen.
         {
 
             var Map = world.Map;
@@ -55,10 +53,11 @@ namespace Projektarbete
             while (true)
             {
                 Map[y, x] = '.';
+                Point temp = new Point(x, y);
                 Console.SetCursorPosition(x, y);
                 var command = Console.ReadKey(true).Key;
                 Console.Write(Map[y, x]);
-
+                
 
                 switch (command)
                 {
@@ -81,13 +80,113 @@ namespace Projektarbete
                     default:
                         break;
                 }
-
+                if (Map[y, x] != '.')
+                {
+                    Entities entity = world.ListOfItemsAndEnemies.Find(e => (e.position.X == x && e.position.Y == y));
+                    if (!Meet(character, entity))
+                    {
+                        x = temp.X;
+                        y = temp.Y;
+                        continue;
+                    }
+                }
                 Map[y, x] = player;
                 
                 Console.SetCursorPosition(x, y);
                 
                 Console.Write(Map[y, x]);
             }
+
+        }
+        static bool Meet(Player player, Entities entity)
+        {
+            bool pass = false;
+            if (entity.GetType().ToString().EndsWith("Enemy"))
+            {
+                if (Fight(player, entity as Enemy))
+                    pass = true;
+            }
+            else
+            {
+                player.PickUp(entity as Item);
+                pass = true;
+            }
+            return pass;
+
+        }
+        static bool Fight(Player player, Enemy enemy)
+        {
+            int counter = 1;
+            Random rng = new Random();
+            while (player.Health > 0 && enemy.Health > 0)
+            {
+                Console.Clear();
+
+                Console.WriteLine($"Round: {counter}" +
+                                   $"\nYou have {player.Health} in health!" +
+                                   $"\nYour enemy has {enemy.Health} in health");
+                Console.WriteLine("Choose your action: \n1. Punch \n2. Kick \n3. Flee");
+                while (true)
+                {
+                    ConsoleKey choice = Console.ReadKey(true).Key;
+
+                    if (choice == ConsoleKey.D1 || choice == ConsoleKey.D2)
+                    {
+                        int damage = 0;
+                        string attackType = "";
+                        if (choice == ConsoleKey.D1)
+                        { damage = player.Punch(rng, enemy.Armour); attackType = "punch"; }
+                        else
+                        { damage = player.Kick(rng, enemy.Armour); attackType = "kick"; }
+
+                        enemy.Health -= damage;
+                        Console.WriteLine($"You {attackType} the enemy, doing {damage} in damage!");
+
+                        while (true)
+                        {
+                            Thread.Sleep(200);
+                            if (enemy.Health > 0)
+                            {
+                                damage = enemy.Punch(rng, player.Armour);
+                                player.Health -= damage;
+                                Console.WriteLine($"The dastardly foe has punched you, inflicting {damage} in damage on you!");
+                                if (attackType == "kick")
+                                { attackType = ""; continue; }
+                                else
+                                    break;
+                            }
+                            else
+                                break;
+                        }
+                        counter++;
+                        break;
+                    }
+
+                    else if (choice == ConsoleKey.D3)
+                    {
+                        if (player.Initiative + rng.Next(1, 7) > enemy.Initiative + rng.Next(1, 7))
+                        {
+                            Console.WriteLine("You have managed to flee!");
+                            Thread.Sleep(1500);
+                            return false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("You did not manage to flee! The fight continues");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Choose 1 or 2");
+                    }
+                }
+                Thread.Sleep(1500);
+            }
+            if (player.Health > 0)
+            { Console.WriteLine("You have bested your foe!"); return true; }
+            else
+            { Console.WriteLine("You have died in shame from a mere punch!"); return false; }
 
         }
     }
