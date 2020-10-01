45010908a8b27c7ab4aec8fa1e2567319863a072
Erik Sundberg <sten.erik.sundberg@gmail.com>
2020-10-01 18:17:43 +0200

    Lade till metoder som behövs för koden som styr vad som händer vid möte.

--- Projektarbete/Player.cs
+++ Projektarbete/Player.cs
@@ -6,8 +6,29 @@ namespace Projektarbete
     class Player : Entities
     {
 
-        List<string> Backpack = new List<string>();
+        List<Item> Backpack = new List<Item>();
 
-
+        public void PickUp(Item item)
+        {
+            Backpack.Add(item);
+        }
+        public int Punch(Random rng, int armour)
+        {
+            int damage = 0;
+            int luckModifier = rng.Next(-3 + Luck, 0);
+            damage = luckModifier + Strength - armour;
+            if (damage < 1)
+                damage = 0;
+            return damage;
+        }
+        public int Kick(Random rng, int armour)
+        {
+            int damage = 0;
+            int luckModifier = rng.Next(-3 + Luck, 0);
+            damage = luckModifier + (Strength * 3 / 2) - armour;
+            if (damage < 1)
+                damage = 0;
+            return damage;
+        }
     }
 }
