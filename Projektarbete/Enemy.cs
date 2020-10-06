4bc13ad13c2a9c30b3951cc11643f94b82beec4b
André Lindqvist <andre.lindqvist82@gmail.com>
2020-10-06 13:42:19 +0200

    .

--- Projektarbete/Enemy.cs
+++ Projektarbete/Enemy.cs
@@ -15,7 +15,7 @@ namespace Projektarbete
             List<int> listOfEnemies = new List<int>();
 
 
-            //Kan vara användbart i framtiden, skapar en lista med 10st objekt av klassen Enemy.
+            //Kan vara användbart i framtiden, skapar en lista med 10st objekt av klassen Enemy..
             for (int i = 0; i < listOfEnemies.Count + 1; i++)
             {
                 var enemy = new Creature();
