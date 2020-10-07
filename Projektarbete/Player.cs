05a7d2b10444c6357e6e91e983efba9e4811ce66
Lucas <lucas.ersson@student.kyh.se>
2020-10-07 09:51:04 +0200

    gjorde tillbaka ändring

--- Projektarbete/Player.cs
+++ Projektarbete/Player.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 
 namespace Projektarbete
@@ -11,24 +10,17 @@ namespace Projektarbete
         public Player (int health, int strength, int armour, int initiative, int luck, Point position, char repChar = '¤'): base (health, strength, armour, initiative, luck, position, repChar)
         {
             Backpack = new List<Item>();
+
         }
         public void PickUp(Item item)
         {
             Backpack.Add(item);
         }
 
-        public void displayBackpack()
+        public static void displayBackpack()
         {
-            int i = 0;
-            Console.SetCursorPosition(63, i);
-            Console.WriteLine("Backpack: ");
-            foreach (var item in Backpack)
-            {
-                i++;
-                Console.SetCursorPosition(63, i);
-                Console.WriteLine(item.Name);
-                
-            }
+            //Tanken är att skapa en foreach loop av innehållet i listan backpack och skriva ut den här,
+            //sedan kalla metoden från StartGame().
         }
 
         
